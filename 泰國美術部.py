dd8d98d10b0e300f80d4a60769917544e789386d
firen8056 <firen8056@gmail.com>
2020-06-10 15:41:49 +0800

    Update 泰國美術部.py

--- 泰國美術部.py
+++ 泰國美術部.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
-
 import googletrans
 import datetime
 import time as t
@@ -37,42 +31,3 @@ for i in newslist:
         link.append('https://www.mkrf.ru' + i.a.get('href'))
     else:
         continue
-
-print(date)
-
-
-# In[ ]:
-
-
-# 分類資訊
-translator = googletrans.Translator()
-title = []
-title_zh = []
-date = []
-link = []
-for i in titledata:
-    title.append(i.text.strip())
-    result = translator.translate(i.text, src='ru', dest = 'zh-tw')
-    title_zh.append(result.text)
-for j in datedata:
-    datetranslate = translator.translate(j.text, src='ru', dest = 'zh-tw')
-    date.append(datetranslate.text.strip())
-for k in linklist:
-    link.append('https://www.mkrf.ru' + k.get('href'))
-    
-# 組成表格
-today = datetime.date.today()
-dict = {'搜尋日期': today,
-        '新聞日期': date,
-        '地區別':'',
-        '國家':'',
-        '發布機構':'',
-        '標題翻譯': title_zh,
-        '標題原文': title,
-        '連結':link
-       }
-df = pd.DataFrame(dict)
-
-# 輸出csv
-df.to_csv(r'C:\Users\User\Desktop\Result.csv', encoding = "utf_8_sig", mode = 'a', index = False)
-
