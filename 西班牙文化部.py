29afd5a6b7623286f35a822c8ab36cde0df4e1cf
firen8056 <firen8056@gmail.com>
2020-06-10 15:42:13 +0800

    Update 西班牙文化部.py

--- 西班牙文化部.py
+++ 西班牙文化部.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[34]:
-
-
 import googletrans
 import datetime
 import time as t
@@ -32,38 +26,3 @@ for i in list:
         link.append('https://www.culturaydeporte.gob.es' + (i.a.get('href')))
     else:
         continue
-
-
-# In[ ]:
-
-
-# 分類資訊
-translator = googletrans.Translator()
-title = []
-title_zh = []
-date = []
-link = []
-for i in titledata:
-    title.append(i.p.text.strip())
-    link.append('https://www.culturaydeporte.gob.es' + (i.a.get('href')))
-    result = translator.translate(i.p.text.strip(), src='es', dest = 'zh-tw')
-    title_zh.append(result.text)
-for j in datedata:
-    date.append(j.text.strip())
-
-# 組成表格
-today = datetime.date.today()
-dict = {'搜尋日期': today,
-        '新聞日期': date,
-        '地區別':'',
-        '國家':'',
-        '發布機構':'西班牙文化部',
-        '標題翻譯': title_zh,
-        '標題原文': title,
-        '連結':link
-       }
-df = pd.DataFrame(dict)
-
-# 輸出csv
-df.to_csv(r'C:\Users\User\Desktop\Result.csv', encoding = "utf_8_sig", mode = 'a', index = False)
-
