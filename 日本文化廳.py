ec7590a02b0517a975c6cf7dc06c4e9cfe10d13f
firen8056 <firen8056@gmail.com>
2020-06-10 15:40:22 +0800

    Update 日本文化廳.py

--- 日本文化廳.py
+++ 日本文化廳.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import googletrans
 import datetime
 import time as t
@@ -35,35 +29,3 @@ for i in everylist:
         date.append(i.find_all('p', class_='news_list_date')[0].text)
     else:
         continue
-
-
-# In[ ]:
-
-
-# 分類資訊
-translator = googletrans.Translator()
-title, link, translate, date = [], [], [], []
-for i in titledata:
-    title.append(i.text.strip())
-    link.append('https://www.bunka.go.jp' + (i.a.get('href')))
-    result = translator.translate(i.text, src='ja', dest = 'zh-tw')
-    title_zh.append(result.text)
-for j in datedata:
-    date.append(j.text.strip())
-
-# 組成表格
-today = datetime.date.today()
-dict = {'搜尋日期': today,
-        '新聞日期': date,
-        '地區別':'',
-        '國家':'',
-        '發布機構':'日本文化廳',
-        '標題翻譯': title_zh,
-        '標題原文': title,
-        '連結':link
-       }
-df = pd.DataFrame(dict)
-
-# 輸出csv
-df.to_csv(r'C:\Users\User\Desktop\Result.csv', encoding = "utf_8_sig", mode = 'a', index = False)
-
